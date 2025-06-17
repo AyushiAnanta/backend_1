7f2c228d70668ee70a2d2d545f9b941bd2a6b2c7
Ananta <theayushiananta@gmail.com>
2025-06-17 18:41:08 +0530

    updation user controls

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -226,9 +226,84 @@ const refreshAccesssToken = asyncHandler(async (req, res) =>{
     }
 })
 
+const changeCurrentPassword = asyncHandler(async(req, res) => {
+    const {oldPassword, newPassword} = req.body
+
+    const user = await User.findById(req.user?._id)
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+
+    if (!isPasswordCorrect) {
+        throw new ApiError(400, "invalid password")
+    }
+
+    user.password = newPassword
+    await user.save({validateBeforeSave: false})
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "password changed successfully"))
+})
+
+const getCurrentUser = asyncHandler(async(req, res) => {
+    return res
+    .status(200)
+    .json(200, req.user, "current user fetched successfully")
+})
+
+const updateAccountDetails = asyncHandler(async(req, res) => {
+    const {fullname, email} = req.body
+
+    if (!fullname || !email) {
+        throw new ApiError(400, "all fields are required")
+        
+    }
+
+    const user = User.findByIdAndUpdate(
+        req.user?._id,
+    {
+        $set: {
+            fullname,
+            email
+        }
+    },
+    {new: true}).select("-password")
+
+    return res.status(200)
+    .json(new ApiResponse(200, user, "account details are updated"))
+})
+
+const updateUserAvatar = asyncHandler(async(req, res) => {
+    const avatarLocalPath = req.file?.path;
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "avatar file missing")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+    if (!avatar.url) {
+        throw new ApiError(400, "error while uploading avatar on cloudinary")
+    }
+
+    await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                avatar: avatar.url
+            }
+        },
+        {new: true}
+    ).select("-password")
+
+
+})
+
 
 export  { 
     registerUser,
     loginUser,
     logoutUser,
-    refreshAccesssToken }
+    refreshAccesssToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails }
